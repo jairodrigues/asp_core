2a26b8df1e15717b9dcd2a3d58cd260c59d057db
Jairo Rodrigues Gonçalves Junior <jgoncalves@acessocard.com.br>
2017-11-08 10:45:30 -0200

    Aula 5

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -3,19 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
 
 namespace CasaDoCodigo.Controllers
 {
     public class PedidoController : Controller
     {
+        List<Produto> produtos = new List<Produto>
+            {
+                new Produto(1, "Sleep not found", 59.90m),
+                new Produto(2, "May the code be with you", 59.90m),
+                new Produto(3, "Rollback", 59.90m),
+                new Produto(4, "REST", 69.90m),
+                new Produto(5, "Design Patterns com Java", 69.90m),
+                new Produto(6, "Vire o jogo com Spring Framework", 69.90m),
+                new Produto(7, "Test-Driven Development", 69.90m),
+                new Produto(8, "iOS: Programe para iPhone e iPad", 69.90m),
+                new Produto(9, "Desenvolvimento de Jogos para Android", 69.90m)
+            };
+
         public IActionResult Carrossel()
         {
-            return View();
+           
+            return View(produtos);
         }
 
         public IActionResult Carrinho()
         {
-            return View();
+            var itensCarrinho = new List<ItemPedido>
+            {
+                new ItemPedido(1, produtos[0],1),
+                new ItemPedido(2, produtos[0],2),
+                new ItemPedido(3, produtos[0],3)
+            };
+
+            CarrinhoViewModel viewModel = new CarrinhoViewModel(itensCarrinho);
+
+            return View(viewModel);
         }
 
         public IActionResult Resumo()
---
+++ CasaDoCodigo/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class Produto
+    {
+        public int Id { get; private set; }
+        public String Nome { get; private set; }
+        public decimal Preco { get; private set; }
+
+        public Produto(int id, String nome, decimal preco)
+        {
+            this.Id = id; ;
+            this.Nome = nome;
+            this.Preco = preco;
+
+        }
+    }
+}
